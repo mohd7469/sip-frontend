792af9860c9848f96c5b4b878704ac40e22b690b
awais786327 <awais3227443@gmail.com>
2020-07-28 19:53:51 +0500

    Angular new app Setup SIP Single click call

---
+++ README.md
@@ -0,0 +1,92 @@
+# Getting started
+
+1. Go to project folder and install dependencies:
+
+```bash
+npm install
+```
+
+2. Launch development server, and open `localhost:4200` in your browser:
+
+```bash
+npm start
+```
+
+# Project structure
+
+```
+dist/                        compiled version
+docs/                        project docs and coding guides
+e2e/                         end-to-end tests
+src/                         project source code
+|- app/                      app components
+|  |- @core/                 core module (singleton services and single-use components)
+|  |- @shared/               shared module  (common components, directives and pipes)
+|  |- app.component.*        app root component (shell)
+|  |- app.module.ts          app root module definition
+|  |- app-routing.module.ts  app routes
+|  +- ...                    additional modules and components
+|- assets/                   app assets (images, fonts, sounds...)
+|- environments/             values for various build environments
+|- theme/                    app global scss variables and theme
+|- translations/             translations files
+|- index.html                html entry point
+|- main.scss                 global style entry point
+|- main.ts                   app entry point
+|- polyfills.ts              polyfills needed by Angular
++- test.ts                   unit tests entry point
+reports/                     test and coverage reports
+proxy.conf.js                backend proxy configuration
+```
+
+## Development server
+
+Run `npm start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change
+any of the source files.
+You should not use `ng serve` directly, as it does not use the backend proxy configuration by default.
+
+# What's in the box
+
+The app template is based on [HTML5](http://whatwg.org/html), [TypeScript](http://www.typescriptlang.org) and
+[Sass](http://sass-lang.com). The translation files use the common [JSON](http://www.json.org) format.
+
+#### Tools
+
+Development, build and quality processes are based on [angular-cli](https://github.com/angular/angular-cli) and
+[NPM scripts](https://docs.npmjs.com/misc/scripts), which includes:
+
+- Optimized build and bundling process with [Webpack](https://webpack.github.io)
+- [Development server](https://webpack.github.io/docs/webpack-dev-server.html) with backend proxy and live reload
+- Cross-browser CSS with [autoprefixer](https://github.com/postcss/autoprefixer) and
+  [browserslist](https://github.com/ai/browserslist)
+- Asset revisioning for [better cache management](https://webpack.github.io/docs/long-term-caching.html)
+- Unit tests using [Jasmine](http://jasmine.github.io) and [Karma](https://karma-runner.github.io)
+- End-to-end tests using [Protractor](https://github.com/angular/protractor)
+- Static code analysis: [TSLint](https://github.com/palantir/tslint), [Codelyzer](https://github.com/mgechev/codelyzer),
+  [Stylelint](http://stylelint.io) and [HTMLHint](http://htmlhint.com/)
+- Local knowledgebase server using [Hads](https://github.com/sinedied/hads)
+- Automatic code formatting with [Prettier](https://prettier.io)
+
+#### Libraries
+
+- [Angular](https://angular.io)
+- [Bootstrap 4](https://getbootstrap.com)
+- [Font Awesome](http://fontawesome.io)
+- [RxJS](http://reactivex.io/rxjs)
+- [ng-bootstrap](https://ng-bootstrap.github.io)
+- [ngx-translate](https://github.com/ngx-translate/core)
+- [Lodash](https://lodash.com)
+
+# Licence
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,251 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+import { Web } from 'sip.js';
+import { getAudio, getButton, getButtons, getInput, getSpan } from './demo-utils';
+// @ts-ignore
+const { SimpleUser, SimpleUserDelegate, SimpleUserOptions } = Web;
+
+declare var $: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+
+export class HomeComponent implements OnInit, AfterViewInit {
+  isLoading = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    // console.log('loading ', this.isLoading);
+  }
+
+  ngAfterViewInit() {
+    const statusSpan = getSpan('status');
+    const serverSpan = getSpan('server');
+    const targetSpan = getSpan('target');
+    // const connectButton = getButton('connect');
+    const callButton = getButton('call');
+    const hangupButton = getButton('hangup');
+    // const disconnectButton = getButton('disconnect');
+    const audioElement = getAudio('remoteAudio');
+    // const keypad = getButtons('keypad');
+    // const dtmfSpan = getSpan('dtmf');
+    const holdCheckbox = getInput('hold');
+    const muteCheckbox = getInput('mute');
+
+    /*config: {
+        password: user.Pass,
+        displayName: user.Display,
+        uri: 'sip:' + user.User + '@' + user.Realm,
+        wsServers: user.WSServer,
+        registerExpires: 30,
+        traceSip: true,
+        log: {
+          level: 0,
+        }
+    },*/
+
+    const user = {
+      'User'      : 'crescas',                    // User Name
+      'Pass'      : 'ab*Nb67f',                   // Password
+      'Realm'     : 'sip.crescas.net',            // Auth Realm
+      'Display'   : 'Crescas',                    // Display Name
+      'WSServer'  : 'wss://sip.crescas.net:4433'  // WebSocket URL
+    };
+
+    const webSocketServer = user.WSServer;
+    serverSpan.innerHTML = webSocketServer;
+
+    const target = 'sip:' + user.User + '@' + user.Realm;
+    targetSpan.innerHTML = target;
+
+    statusSpan.innerHTML = 'Connecting..';
+
+    audioElement.hidden = true;
+
+    const displayName = user.Display;
+
+    // @ts-ignore
+    const simpleUserDelegate: SimpleUserDelegate = {
+      onCallCreated: (): void => {
+        console.log(`[${displayName}] Call created`);
+        callButton.disabled = true;
+        hangupButton.disabled = false;
+        // keypadDisabled(true);
+        holdCheckboxDisabled(true);
+        muteCheckboxDisabled(true);
+      },
+      onCallAnswered: (): void => {
+        console.log(`[${displayName}] Call answered`);
+        // keypadDisabled(false);
+        holdCheckboxDisabled(false);
+        muteCheckboxDisabled(false);
+      },
+      onCallHangup: (): void => {
+        console.log(`[${displayName}] Call hangup`);
+        callButton.disabled = false;
+        hangupButton.disabled = true;
+        // keypadDisabled(true);
+        holdCheckboxDisabled(true);
+        muteCheckboxDisabled(true);
+      },
+      onCallHold: (held: boolean): void => {
+        console.log(`[${displayName}] Call hold ${held}`);
+        holdCheckbox.checked = held;
+      }
+    };
+
+    // @ts-ignore
+    const simpleUserOptions: SimpleUserOptions = {
+      delegate: simpleUserDelegate,
+      media: {
+        remote: {
+          audio: audioElement
+        }
+      },
+      userAgentOptions: {
+        displayName,
+        authorizationUsername: user.User,
+        authorizationPassword: user.Pass
+      }
+    };
+
+    const simpleUser = new SimpleUser(webSocketServer, simpleUserOptions);
+
+    // connectButton.addEventListener('click', () => {
+      // connectButton.disabled = true;
+      // disconnectButton.disabled = true;
+      callButton.disabled = true;
+      hangupButton.disabled = true;
+      simpleUser
+        .connect()
+        .then(() => {
+          // connectButton.disabled = true;
+          // disconnectButton.disabled = false;
+          callButton.disabled = false;
+          hangupButton.disabled = true;
+          statusSpan.innerHTML = 'Connected';
+        })
+        .catch((error: Error) => {
+          // connectButton.disabled = false;
+          console.error(`[${simpleUser.id}] failed to connect`);
+          console.error(error);
+          alert('Failed to connect.\n' + error);
+        });
+    // });
+
+    callButton.addEventListener('click', () => {
+      callButton.disabled = true;
+      hangupButton.disabled = true;
+      simpleUser.call(target).catch((error: Error) => {
+        console.error(`[${simpleUser.id}] failed to place call`);
+        console.error(error);
+        alert('Failed to place call.\n' + error);
+      });
+    });
+
+    hangupButton.addEventListener('click', () => {
+      callButton.disabled = true;
+      hangupButton.disabled = true;
+      simpleUser.hangup().catch((error: Error) => {
+        console.error(`[${simpleUser.id}] failed to hangup call`);
+        console.error(error);
+        alert('Failed to hangup call.\n' + error);
+      });
+    });
+
+    // disconnectButton.addEventListener('click', () => {
+    //   // connectButton.disabled = true;
+    //   // disconnectButton.disabled = true;
+    //   callButton.disabled = true;
+    //   hangupButton.disabled = true;
+    //   simpleUser
+    //     .disconnect()
+    //     .then(() => {
+    //       // connectButton.disabled = false;
+    //       // disconnectButton.disabled = true;
+    //       callButton.disabled = true;
+    //       hangupButton.disabled = true;
+    //     })
+    //     .catch((error: Error) => {
+    //       console.error(`[${simpleUser.id}] failed to disconnect`);
+    //       console.error(error);
+    //       alert('Failed to disconnect.\n' + error);
+    //     });
+    // });
+
+    // keypad.forEach((button) => {
+    //   button.addEventListener('click', () => {
+    //     const tone = button.textContent;
+    //     if (tone) {
+    //       simpleUser.sendDTMF(tone).then(() => {
+    //         dtmfSpan.innerHTML += tone;
+    //       });
+    //     }
+    //   });
+    // });
+
+    // const keypadDisabled = (disabled: boolean): void => {
+    //   keypad.forEach((button) => (button.disabled = disabled));
+    //   dtmfSpan.innerHTML = '';
+    // };
+
+    holdCheckbox.addEventListener('change', () => {
+      if (holdCheckbox.checked) {
+        // Checkbox is checked..
+        simpleUser.hold().catch((error: Error) => {
+          holdCheckbox.checked = false;
+          console.error(`[${simpleUser.id}] failed to hold call`);
+          console.error(error);
+          alert('Failed to hold call.\n' + error);
+        });
+      } else {
+        // Checkbox is not checked..
+        simpleUser.unhold().catch((error: Error) => {
+          holdCheckbox.checked = true;
+          console.error(`[${simpleUser.id}] failed to unhold call`);
+          console.error(error);
+          alert('Failed to unhold call.\n' + error);
+        });
+      }
+    });
+
+    const holdCheckboxDisabled = (disabled: boolean): void => {
+      holdCheckbox.checked = false;
+      holdCheckbox.disabled = disabled;
+    };
+
+    muteCheckbox.addEventListener('change', () => {
+      if (muteCheckbox.checked) {
+        // Checkbox is checked..
+        simpleUser.mute();
+        if (simpleUser.isMuted() === false) {
+          muteCheckbox.checked = false;
+          console.error(`[${simpleUser.id}] failed to mute call`);
+          alert('Failed to mute call.\n');
+        }
+      } else {
+        // Checkbox is not checked..
+        simpleUser.unmute();
+        if (simpleUser.isMuted() === true) {
+          muteCheckbox.checked = true;
+          console.error(`[${simpleUser.id}] failed to unmute call`);
+          alert('Failed to unmute call.\n');
+        }
+      }
+    });
+
+    const muteCheckboxDisabled = (disabled: boolean): void => {
+      muteCheckbox.checked = false;
+      muteCheckbox.disabled = disabled;
+    };
+
+    // connectButton.disabled = false;
+
+  }
+
+}
